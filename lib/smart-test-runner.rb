446a07d378a47ec01daac64c33480cf4b2c7a84a
Marten Veldthuis <marten@veldthuis.com>
2011-10-14 21:56:46 +0200

    Add initial version of SmartTestHelper

---
+++ lib/smart-test-runner.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+# Put this in the script/ directory of your Rails app, then run it with a spec
+# filename. If the spec uses spec_helper, it'll be run inside Bundler.
+# Otherwise, it'll be run directly with whatever `rspec` executable is on the
+# path.
+
+class SmartTestRunner
+  def self.command_to_run(path = nil)
+    path = "spec" unless path
+    
+    `grep -r 'spec_helper' #{path}`
+    if $?.exitstatus == 1 # no match; we have a stand-alone spec
+      "rspec #{path}"
+    else
+      "bundle exec rspec #{path}"
+    end
+  end
+end
+
+#need_rails=1
+
+#if [ $# -gt 0 ]; then # we have args
+    #filename=$1
+    ## Remove trailing line numbers from filename, e.g. spec/my_spec.rb:33
+    #grep_filename=`echo $1 | sed 's/:.*$//g'`
+
+    #(set +e; grep -r 'spec_helper' $grep_filename; echo '') > /dev/null
+    #if [ $? -eq 1 ]; then # no match; we have a stand-alone spec
+        #need_rails=''
+    #fi
+#else # we have no args
+    #filename='spec'
+#fi
+
+#command='rspec'
+
+#if [ $need_rails ]; then
+    #command="ruby -S bundle exec $command"
+#fi
+
+#RAILS_ENV=test $command $filename
--- spec/smart-test-runner_spec.rb
+++ spec/smart-test-runner_spec.rb
@@ -1,7 +1,30 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+CURRENT_PWD   = Dir.pwd
+FIXTURES_ROOT = File.expand_path(File.dirname(__FILE__) + '/fixtures')
 
 describe "SmartTestRunner" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  context "for a ruby project" do
+    context "using rspec" do
+      before(:all) { Dir.chdir(FIXTURES_ROOT + '/ruby/rspec') }
+      after(:all)  { Dir.chdir(CURRENT_PWD) }
+
+      it "should return 'bundle exec rspec spec' when run with no arguments" do
+        SmartTestRunner.command_to_run().should == "bundle exec rspec spec"
+      end
+
+      it "should return 'bundle exec rspec spec/with_spec_helper_spec.rb'" do
+        SmartTestRunner.command_to_run('spec/with_spec_helper_spec.rb').should == "bundle exec rspec spec/with_spec_helper_spec.rb"
+      end
+
+      it "should return 'rspec spec/without_spec_helper_spec.rb'" do
+        SmartTestRunner.command_to_run('spec/without_spec_helper_spec.rb').should == "rspec spec/without_spec_helper_spec.rb"
+      end
+
+      it "should not use bundler when no Gemfile exists"
+      it "should not call rspec outside of bundler when the rspec command does not exist"
+    end
+    
+    context "using minitest"
   end
+
+  context "for a python project"
 end
