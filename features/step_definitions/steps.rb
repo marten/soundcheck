b4b1b6d9d1d1e64f5f9a14199a2a84c733e5a644
Marten Veldthuis <marten@veldthuis.com>
2011-10-17 12:09:04 +0200

    Fix that features would use soundcheck gem instead of local files

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,9 +1,11 @@
+SOUNDCHECK_ROOT = File.expand_path(File.join(File.dirname(__FILE__), "..", ".."))
+
 Given /^the "([^"]*)" fixture$/ do |fixture_path|
-  Dir.chdir(File.join(File.expand_path(File.dirname(__FILE__)), "../../spec/fixtures", fixture_path))
+  Dir.chdir(File.join(SOUNDCHECK_ROOT, "spec", "fixtures", fixture_path))
 end
 
 When /^I run soundcheck with "([^"]*)"$/ do |spec_file|
-  puts output = `#{File.join(File.expand_path(File.dirname(__FILE__)), "../../bin/soundcheck")} #{spec_file}`
+  puts output = `ruby -I#{File.join(SOUNDCHECK_ROOT, "lib")} #{File.join(SOUNDCHECK_ROOT, "bin", "soundcheck")} #{spec_file}`
 end
 
 Then /^it should have passed$/ do
