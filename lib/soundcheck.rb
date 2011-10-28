edc7bdaf11204a9d422e416d479933fa54761f56
Marten Veldthuis <marten@veldthuis.com>
2011-10-28 19:57:46 +0200

    Cleanup old comments

--- lib/soundcheck.rb
+++ lib/soundcheck.rb
@@ -1,9 +1,3 @@
-#!/usr/bin/env ruby
-# Put this in the script/ directory of your Rails app, then run it with a spec
-# filename. If the spec uses spec_helper, it'll be run inside Bundler.
-# Otherwise, it'll be run directly with whatever `rspec` executable is on the
-# path.
-
 class Soundcheck
   attr_accessor :path
 
@@ -24,26 +18,3 @@ class Soundcheck
     $?.exitstatus == 1 # no match; we have a stand-alone spec
   end
 end
-
-#need_rails=1
-
-#if [ $# -gt 0 ]; then # we have args
-    #filename=$1
-    ## Remove trailing line numbers from filename, e.g. spec/my_spec.rb:33
-    #grep_filename=`echo $1 | sed 's/:.*$//g'`
-
-    #(set +e; grep -r 'spec_helper' $grep_filename; echo '') > /dev/null
-    #if [ $? -eq 1 ]; then # no match; we have a stand-alone spec
-        #need_rails=''
-    #fi
-#else # we have no args
-    #filename='spec'
-#fi
-
-#command='rspec'
-
-#if [ $need_rails ]; then
-    #command="ruby -S bundle exec $command"
-#fi
-
-#RAILS_ENV=test $command $filename
