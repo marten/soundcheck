f37bef1c2f7478e94b9f4adecab716f90eb50a91
Marten Veldthuis <marten@veldthuis.com>
2011-10-28 19:58:31 +0200

    Don't run bundle exec when no Gemfile exists

--- lib/soundcheck.rb
+++ lib/soundcheck.rb
@@ -6,15 +6,24 @@ class Soundcheck
   end
 
   def command_to_run
-    if requires_spec_helper?
-      "rspec #{@path}"
-    else
-      "bundle exec rspec #{@path}"
+    if has_gemfile?
+      if requires_spec_helper?
+        return "rspec #{@path}"
+      else
+        return "bundle exec rspec #{@path}"
+      end
     end
+
+    # Assume rspec
+    "rspec #{@path}"
   end
 
   def requires_spec_helper?
     `grep -r 'spec_helper' #{@path}`
     $?.exitstatus == 1 # no match; we have a stand-alone spec
+  end
+
+  def has_gemfile?
+    File.exist?("Gemfile")
   end
 end
--- spec/soundcheck_spec.rb
+++ spec/soundcheck_spec.rb
@@ -3,29 +3,38 @@ require File.expand_path(File.dirname(__FILE__) + '/../lib/soundcheck')
 CURRENT_PWD   = Dir.pwd
 FIXTURES_ROOT = File.expand_path(File.dirname(__FILE__) + '/../fixtures')
 
+def cmd_for(path = nil)
+  Soundcheck.new(path).command_to_run
+end
+
 describe "Soundcheck" do
   context "for a ruby project" do
     context "using rspec" do
-      before(:all) { Dir.chdir(FIXTURES_ROOT + '/ruby/rspec') }
-      after(:all)  { Dir.chdir(CURRENT_PWD) }
+      before(:all) { Dir.chdir(FIXTURES_ROOT + '/ruby-rspec') }
 
-      it "should return 'bundle exec rspec spec' when run with no arguments" do
-        Soundcheck.new().command_to_run.should == "bundle exec rspec spec"
+      it "should not use bundler when no Gemfile exists" do
+        cmd_for.should == "rspec spec"
       end
 
-      it "should return 'bundle exec rspec spec/with_spec_helper_spec.rb'" do
-        Soundcheck.new('spec/with_spec_helper_spec.rb').command_to_run.should == "bundle exec rspec spec/with_spec_helper_spec.rb"
-      end
+      context "using bundler" do
+        before(:all) { Dir.chdir(FIXTURES_ROOT + '/ruby-bundler-rspec') }
 
-      it "should return 'rspec spec/without_spec_helper_spec.rb'" do
-        Soundcheck.new('spec/without_spec_helper_spec.rb').command_to_run.should == "rspec spec/without_spec_helper_spec.rb"
-      end
+        it "should return 'bundle exec rspec spec' when run with no arguments" do
+          cmd_for.should == "bundle exec rspec spec"
+        end
 
-      it "should not use bundler when no Gemfile exists"
-      it "should not call rspec outside of bundler when the rspec command does not exist"
+        it "should return 'bundle exec rspec spec/with_spec_helper_spec.rb'" do
+          cmd_for('spec/with_spec_helper_spec.rb').should == "bundle exec rspec spec/with_spec_helper_spec.rb"
+        end
+
+        it "should return 'rspec spec/without_spec_helper_spec.rb'" do
+          cmd_for('spec/without_spec_helper_spec.rb').should == "rspec spec/without_spec_helper_spec.rb"
+        end
+      end
     end
     
     context "using minitest"
+    context "using test::unit"
   end
 
   context "for a python project"
