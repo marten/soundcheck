d32569fa573b55c275295d96cef39dd1c21f0e87
Marten Veldthuis <marten@veldthuis.com>
2011-10-30 22:35:00 +0100

    Add command line options parsing

--- features/soundcheck.feature
+++ features/soundcheck.feature
@@ -2,7 +2,11 @@ Feature: Soundcheck
   In order to run my tests quickly
   I want to require as little as possible
 
+  Scenario: Command line help
+    When I run "soundcheck --help"
+    Then I should see "Usage: soundcheck"
+
   Scenario: Ruby and RSpec
     Given the "ruby-bundler-rspec" fixture
-    When I run soundcheck with "spec"
+    When I run "soundcheck spec"
     Then it should have passed
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -4,10 +4,14 @@ Given /^the "([^"]*)" fixture$/ do |fixture_path|
   Dir.chdir(File.join(SOUNDCHECK_ROOT, "fixtures", fixture_path))
 end
 
-When /^I run soundcheck with "([^"]*)"$/ do |spec_file|
-  puts output = `ruby -I#{File.join(SOUNDCHECK_ROOT, "lib")} #{File.join(SOUNDCHECK_ROOT, "bin", "soundcheck")} #{spec_file}`
+When /^I run "soundcheck ([^"]*)"$/ do |args|
+  @output = `ruby -I#{File.join(SOUNDCHECK_ROOT, "lib")} #{File.join(SOUNDCHECK_ROOT, "bin", "soundcheck")} #{args}`
 end
 
 Then /^it should have passed$/ do
   $?.exitstatus.should == 0
+end
+
+Then /^I should see "([^"]*)"$/ do |text|
+  @output.should include(text)
 end
